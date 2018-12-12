ff825a6e48bfd63ec8bbc97ab197317e45fda98c
SergeyK-WebDeveloper <Sergey.Kist@gmail.com>
2018-12-12 20:44:12 +0000

    Yii2  13__Сделан Поиск

--- 00__Yii2 (web store dev)/controllers/CategoryController.php
+++ 00__Yii2 (web store dev)/controllers/CategoryController.php
@@ -21,8 +21,7 @@ class CategoryController extends AppController{
     }
 
     public function actionView($id){
-        $id = Yii::$app->request->get('id');
-
+//        $id = Yii::$app->request->get('id');
         $category = Category::findOne($id);
         if(empty($category))
             throw new \yii\web\HttpException(404, 'Такой категории нет');
@@ -33,6 +32,17 @@ class CategoryController extends AppController{
         $products = $query->offset($pages->offset)->limit($pages->limit)->all();
         $this->setMeta('E-SHOPPER | ' . $category->name, $category->keywords, $category->description);
         return $this->render('view', compact('products', 'pages', 'category'));
+    }
+
+    public function actionSearch(){
+        $q = trim(Yii::$app->request->get('q'));
+        $this->setMeta('E-SHOPPER | Поиск: ' . $q);
+        if(!$q)
+            return $this->render('search');
+        $query = Product::find()->where(['like', 'name', $q]);
+        $pages = new Pagination(['totalCount' => $query->count(), 'pageSize' => 3, 'forcePageParam' => false, 'pageSizeParam' => false]);
+        $products = $query->offset($pages->offset)->limit($pages->limit)->all();
+        return $this->render('search', compact('products', 'pages', 'q'));
     }
 
 } 
