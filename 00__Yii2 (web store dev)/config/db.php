c280424a3d628d49dc1f47b480484f40430f09a0
SergeyK-WebDeveloper <Sergey.Kist@gmail.com>
2019-04-24 19:24:42 +0000

    Laravel - Landing Page

--- 00__Yii2 (web store dev)/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2_loc',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-];
